e0ee67042f19d0c2654f7675c66cfb4e391f443e
FabiánSpina <fabianspina_mfs@hotmail.com>
2025-09-15 02:17:52 -0300

    Create adornos.h

---
+++ misc/adornos.h
@@ -0,0 +1,10 @@
+#ifndef _ADORNOS_H
+#define _ADORNOS_H
+#include <windows.h>
+#include <iostream>
+using namespace std;
+#define borrar system("cls")
+#define decir cout <<
+void beep_pieza();
+
+#endif // _ADORNOS_H
