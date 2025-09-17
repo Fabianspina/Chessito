07b1d6208342a451f52faf40d26ed0976161a40e
FabiánSpina <fabianspina_mfs@hotmail.com>
2025-09-16 22:35:42 -0300

    Create valida.h

---
+++ valida.h
@@ -0,0 +1,18 @@
+#ifndef _VALIDA_H
+#define _VALIDA_H
+#include <iostream>
+using namespace std;
+#include "tc.h"
+#include "convierte.h"
+#include "turno.h"
+#include "rey.h"
+#define decirn cout <<"\n"
+void actualiza();
+void eligepieza();
+void actualizapeonalp1();
+void actualizapeonalp2();
+void actualizapeonalp3();
+void actualizapeonalp4();
+void coronar();
+
+#endif // _VALIDA_H
