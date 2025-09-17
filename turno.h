48a4409280bcad9e3ac26ac26d1afddf9e67a7fd
FabiánSpina <fabianspina_mfs@hotmail.com>
2025-09-16 22:35:52 -0300

    Create turno.h

---
+++ turno.h
@@ -0,0 +1,14 @@
+#ifndef TURNO_H
+#define TURNO_H
+#include <windows.h>
+#include <iostream>
+using namespace std;
+#define decir cout <<
+#define borrar system("cls")
+#define pausa system("pause >> misc/null")
+int turno();
+extern int cont;
+extern int m;
+extern int turn;
+#endif // TURNO_H
+
