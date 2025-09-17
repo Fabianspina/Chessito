892844f06c27e0d181cc4c37ceffb81cec058509
FabiánSpina <fabianspina_mfs@hotmail.com>
2025-09-16 22:25:48 -0300

    Create convierte.h

---
+++ convierte.h
@@ -0,0 +1,12 @@
+#ifndef CONVIERTE_H
+#define CONVIERTE_H
+extern char cas[2];
+extern char casdes[2];
+extern short int ori;
+extern short int dest;
+int indiceDesdeXY(int x, int y);
+void obtenerXY(int i, int& x, int& y);
+void conviertedestino();
+void convierteorigen();
+
+#endif // CONVIERTE_H
