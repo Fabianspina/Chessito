fc6c22014665e0f3398b3f00ede10bc33a71e0d8
FabiánSpina <fabianspina_mfs@hotmail.com>
2025-09-16 22:35:38 -0300

    Create presentacion.h

---
+++ presentacion.h
@@ -0,0 +1,11 @@
+#ifndef _PRESENTACION_H_
+#define _PRESENTACION_H_
+#include <windows.h>
+void presentacion();
+/*
+* presentacion.h es sólo la presentación del juego. No influye en el funcionamiento del programa
+*/
+COORD coord={0,0};
+void gotoxy(int x, int y);
+
+#endif // _PRESENTACION_H_
